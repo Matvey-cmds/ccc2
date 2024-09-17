60e773b4644e7c8ccb241491ec631fa67c9fe89f
Matvey-cmds <ramond.crystaler@gmail.com>
2024-09-17 15:45:29 +1000

    111

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace ccc2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Программа по расчету суммы вух чисел");
+            Console.WriteLine("введите первое число");
+            int  a = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите второе число");
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("сумма двух чисел равна" + c);
+
         }
     }
 }
